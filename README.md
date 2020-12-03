417e45f66bd212f626842c462518f9a278395ba9
Juri Merello <juri.merello@gmail.com>
2020-12-03 18:21:55 +0000

    Hai fatto in modo che puoi comprare con teste moneta server prestabilite e con blocchi di qualsiasi tipo di minecraft, avendo a disposizione anche il suggerimento di un datalist. Invece gli oggetti comprati possono essere: teste moneta server, teste dal sito delle teste e qualunque blocco di minecraft.
    Inoltre hai aggiunto la possibilita' di aggiungere quanti trade vuoi direttamente con un click, ad esempio 7 in una botta sola.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 ISTRUZIONI PER L'USO
 
+- Tutti i campi vanno compilati ad eccezzione di quelli facoltativi
+- Nel campo "Tipo di Moneta" si possono selezionare sia le "teste-moneta" che useremo nel server, sia
+  i normali elementi di minecraft
+- Nel campo "Oggetto in vendita" si possono selezionare sia le "teste-moneta" che useremo nel server, sia
+  i normali elementi di minecraft, sia le teste che si trovano nel sito sottostante. (Per immettere
+  correttamente la testa selezionata dal sito, basta incollare il link 1.16 suggerito dal
+  sito stesso nella sua interezza)
 - Per sciegliere le teste andare su: https://minecraft-heads.com/
--- Resources/JS/logic.js
+++ Resources/JS/logic.js
@@ -7,7 +7,8 @@ let counter = 0; // Serve in seguito per attribuire gli ID corretti
 
 // Triggeratori (il primo crea l'HTML degli scambi incrementando un counter che incrementera' tutti gli ID)
 addTradeButton.onclick = () => {
-  let trade = `<div class="trade n${counter}">
+  function trade() {
+    let trade = `<div class="trade n${counter}">
             <br/>
             <hr align="left" size="1" width="50" color="gray" noshade />
             <div class="inputInfo">
@@ -16,28 +17,13 @@ addTradeButton.onclick = () => {
             </div>
             <div class="inputInfo">
               <label for="Tipo-Moneta-${counter}">Tipo moneta</label>
-              <select name="" id="Tipo-Moneta-${counter}">
-                <option
-                  value='display:{Name:"{\\"text\\":\\"Iron Block (Beta)\\"}"},SkullOwner:{Id:[I;-1273404245,-1758966597,-1508271075,-833688128],Properties:{textures:[{Value:"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNjU2MjljMWM3N2FlYTJiMGNlYmNmMzMzNjU1ZTY4ZGIxMzRmNDg0MWMwOGQ5ZTg3NWMzMDc0YWMzMGUyYTZkZSJ9fX0="}]}}'
-                >
-                  ferro
-                </option>
-                <option
-                  value='display:{Name:"{\\"text\\":\\"Golden Blank\\"}"},SkullOwner:{Id:[I;-2055602587,-531214450,-1555823654,-1254945024],Properties:{textures:[{Value:"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMzUxMTM3ZTExNDQzYThmYmIwNWZjZDNjY2MxYWY5YmQyMzAzOTE4ZjM1NDQ4MTg1ZTNlZDk2ZWYxODRkYSJ9fX0="}]}}'
-                >
-                  oro
-                </option>
-                <option
-                  value='display:{Name:"{\\"text\\":\\"Diamond Block\\"}"},SkullOwner:{Id:[I;189941930,1156075107,-1303444300,-811366351],Properties:{textures:[{Value:"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMjM0NjMwOWRiYjRhYWI2ZjlkMTRhNjI3ZmI3M2Y0ODE0MTY1ODM3YjQyMzg1ZjA2NDMyZDY4MDIzYTg0NDVkIn19fQ=="}]}}'
-                >
-                  diamante
-                </option>
-                <option
-                  value='display:{Name:"{\\"text\\":\\"Block of Netherite\\"}"},SkullOwner:{Id:[I;1156551679,-1088599583,-1500868190,-421557710],Properties:{textures:[{Value:"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMjc5NTdmODk1ZDdiYzUzNDIzYTM1YWFjNTlkNTg0YjQxY2MzMGUwNDAyNjljOTU1ZTQ1MWZlNjgwYTFjYzA0OSJ9fX0="}]}}'
-                >
-                  netherite
-                </option>
-              </select>
+              <input list="moneta-${counter}" id="Tipo-Moneta-${counter}">
+                <datalist id="moneta-${counter}">
+                  <option value="Testa Ferro" />
+                  <option value="Testa Oro" />
+                  <option value="Testa Diamante" />
+                  <option value="Testa Netherite" />
+                </datalist>
             </div>
             <div class="inputInfo">
               <label for="Quantità-Monete-${counter}">Quantità monete</label>
@@ -45,7 +31,13 @@ addTradeButton.onclick = () => {
             </div>
             <div class="inputInfo">
               <label for="Oggetto-Vendita-${counter}">Oggetto in vendita</label>
-              <input type="text" id="Oggetto-Vendita-${counter}" />
+              <input list="prodotto-${counter}" id="Oggetto-Vendita-${counter}">
+                <datalist id="prodotto-${counter}">
+                  <option value="Testa Ferro" />
+                  <option value="Testa Oro" />
+                  <option value="Testa Diamante" />
+                  <option value="Testa Netherite" />
+                </datalist>
             </div>
             <div class="inputInfo">
               <label for="Quantità-Oggetti-Venduti-${counter}"
@@ -56,8 +48,23 @@ addTradeButton.onclick = () => {
             <button class="removeTrade" id="removeTradeButton-${counter}" onclick="changeDisplay(${counter})">Rimuovi</button>
             <br />
           </div>`;
-  $(".trades").append(trade);
-  counter++;
+    $(".trades").append(trade);
+    for (let item of itemID) {
+      $(`#moneta-${counter}`).append('<option value="' + item + '"></option>');
+      $(`#prodotto-${counter}`).append(
+        '<option value="' + item + '"></option>'
+      );
+    }
+    counter++;
+  }
+  // Questo IF - ELSE serve a mettere un trade alla volta oppure ad aggiungerne in un colpo il numero selezionato
+  if ($("#numberOfTrades").val() === "") {
+    trade();
+  } else {
+    for (let i = 0; i < parseInt($("#numberOfTrades").val()); i++) {
+      trade();
+    }
+  }
 };
 
 triggerButton.onclick = () => {
@@ -67,6 +74,7 @@ triggerButton.onclick = () => {
 //-------------------------//
 
 // Funzioni
+// Funzione che crea il comando da incollare nel command block
 function createCommand() {
   let villagerPosition = document.getElementById("posizione").value;
   let villagerProfession = document.getElementById("Professione").value;
@@ -87,23 +95,59 @@ function createCommand() {
     let soldItem;
 
     // Se item e' una testa presa dal sito allora la IF qui sotto lo vede e affida a item solo la porzione di stringa che serve a noi,
-    // diversamente il codice non e' ancora stato implementato
     if (soldItemInput.slice(0, 5) === "/give") {
       // /give @p minecraft:player_head{...} 1     Questo e' quello che prendi dal sito delle teste,
       // quello che realmente ti interessa e' al posto dei puntini in mezzo alle graffe
       soldItem = soldItemInput.slice(31, -3);
-    } // qui puoi aggiungere un else o un else if per tutti gli altri casi
-
-    // Se buyItem non e' una testa, per ora, per come e' scritto il codice, dovrai fare modifiche...
-    // allo stesso modo in futuro potresti implementare diversi modi per comprare oltre a quelli che hai messo forzatamente (ferro, oro, diamante, netherite).
+    } else {
+      soldItem = soldItemInput;
+    }
 
     // Creiamo un controllo per vedere se un trade e' stato annullato, se $('.n'+i)[0].attributes[1] non fosse === a undefined,
     // vorrebbe dire che all'interno ci sarebbe una proprietà cosi' $('.n6')[0].attributes[1].value === 'display: none;' e se fosse,
     // allora vogliamo che il trade non sia preso in considerazione.
     if ($(".n" + i)[0].attributes[1] === undefined) {
-      // Se vorrai creare scambi fra tutti gli oggetti e non solo teste allora le stringhe sotto vanno modificate
-      let recipeBuy = `{buy:{id:"minecraft:player_head", Count:${quantityBuy}b, tag:{${buyItem}}},`;
-      let recipeSell = `sell:{id:"minecraft:player_head", Count:${quantitySold}b, tag:{${soldItem}}},rewardExp:0b,maxUses:9999999}`;
+      /* Esempi di villager e relativo trade:
+
+        /summon villager ~ ~ ~ {Offers:{Recipes:[{buy:{id:"minecraft:stone",Count:64b},sell:{id:"minecraft:diamond",Count:2b}}]}}
+        {buy:{id:"minecraft:stone",Count:64b},sell:{id:"minecraft:diamond",Count:2b}}
+
+        /summon villager ~ ~ ~ {Offers:{Recipes:[{buy:{id:"minecraft:stone",Count:64b},sell:{id:"minecraft:diamond",Count:2b}},{buy:{id:'minecraft:stone',Count:64b},sell:{id:'minecraft:diamond',Count:2b}}]}}
+        {buy:{id:"minecraft:stone",Count:64b},sell:{id:"minecraft:diamond",Count:2b}},{buy:{id:'minecraft:stone',Count:64b},sell:{id:'minecraft:diamond',Count:2b}}
+
+      */
+
+      let recipeSell;
+      let recipeBuy;
+
+      if (buyItem.slice(0, 7) === "display") {
+        recipeBuy = `{buy:{id:"minecraft:player_head", Count:${quantityBuy}b, tag:{${buyItem}}},`;
+      } else if (buyItem === "Testa Ferro") {
+        recipeBuy = `{buy:{id:"minecraft:player_head", Count:${quantityBuy}b, tag:{display:{Name:"{\\"text\\":\\"Iron Block (Beta)\\"}"},SkullOwner:{Id:[I;-1273404245,-1758966597,-1508271075,-833688128],Properties:{textures:[{Value:"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNjU2MjljMWM3N2FlYTJiMGNlYmNmMzMzNjU1ZTY4ZGIxMzRmNDg0MWMwOGQ5ZTg3NWMzMDc0YWMzMGUyYTZkZSJ9fX0="}]}}}},`;
+      } else if (buyItem === "Testa Oro") {
+        recipeBuy = `{buy:{id:"minecraft:player_head", Count:${quantityBuy}b, tag:{display:{Name:"{\\"text\\":\\"Golden Blank\\"}"},SkullOwner:{Id:[I;-2055602587,-531214450,-1555823654,-1254945024],Properties:{textures:[{Value:"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMzUxMTM3ZTExNDQzYThmYmIwNWZjZDNjY2MxYWY5YmQyMzAzOTE4ZjM1NDQ4MTg1ZTNlZDk2ZWYxODRkYSJ9fX0="}]}}}},`;
+      } else if (buyItem === "Testa Diamante") {
+        recipeBuy = `{buy:{id:"minecraft:player_head", Count:${quantityBuy}b, tag:{display:{Name:"{\\"text\\":\\"Diamond Block\\"}"},SkullOwner:{Id:[I;189941930,1156075107,-1303444300,-811366351],Properties:{textures:[{Value:"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMjM0NjMwOWRiYjRhYWI2ZjlkMTRhNjI3ZmI3M2Y0ODE0MTY1ODM3YjQyMzg1ZjA2NDMyZDY4MDIzYTg0NDVkIn19fQ=="}]}}}},`;
+      } else if (buyItem === "Testa Netherite") {
+        recipeBuy = `{buy:{id:"minecraft:player_head", Count:${quantityBuy}b, tag:{display:{Name:"{\\"text\\":\\"Block of Netherite\\"}"},SkullOwner:{Id:[I;1156551679,-1088599583,-1500868190,-421557710],Properties:{textures:[{Value:"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMjc5NTdmODk1ZDdiYzUzNDIzYTM1YWFjNTlkNTg0YjQxY2MzMGUwNDAyNjljOTU1ZTQ1MWZlNjgwYTFjYzA0OSJ9fX0="}]}}}},`;
+      } else {
+        recipeBuy = `{buy:{id:"${buyItem}", Count:${quantityBuy}b},`;
+      }
+      //---
+      if (soldItem.slice(0, 7) === "display") {
+        recipeSell = `sell:{id:"minecraft:player_head", Count:${quantitySold}b, tag:{${soldItem}}},rewardExp:0b,maxUses:9999999}`;
+      } else if (soldItem === "Testa Ferro") {
+        recipeSell = `sell:{id:"minecraft:player_head", Count:${quantitySold}b, tag:{display:{Name:"{\\"text\\":\\"Iron Block (Beta)\\"}"},SkullOwner:{Id:[I;-1273404245,-1758966597,-1508271075,-833688128],Properties:{textures:[{Value:"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNjU2MjljMWM3N2FlYTJiMGNlYmNmMzMzNjU1ZTY4ZGIxMzRmNDg0MWMwOGQ5ZTg3NWMzMDc0YWMzMGUyYTZkZSJ9fX0="}]}}}},rewardExp:0b,maxUses:9999999}`;
+      } else if (soldItem === "Testa Oro") {
+        recipeSell = `sell:{id:"minecraft:player_head", Count:${quantitySold}b, tag:{display:{Name:"{\\"text\\":\\"Golden Blank\\"}"},SkullOwner:{Id:[I;-2055602587,-531214450,-1555823654,-1254945024],Properties:{textures:[{Value:"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMzUxMTM3ZTExNDQzYThmYmIwNWZjZDNjY2MxYWY5YmQyMzAzOTE4ZjM1NDQ4MTg1ZTNlZDk2ZWYxODRkYSJ9fX0="}]}}}},rewardExp:0b,maxUses:9999999}`;
+      } else if (soldItem === "Testa Diamante") {
+        recipeSell = `sell:{id:"minecraft:player_head", Count:${quantitySold}b, tag:{display:{Name:"{\\"text\\":\\"Diamond Block\\"}"},SkullOwner:{Id:[I;189941930,1156075107,-1303444300,-811366351],Properties:{textures:[{Value:"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMjM0NjMwOWRiYjRhYWI2ZjlkMTRhNjI3ZmI3M2Y0ODE0MTY1ODM3YjQyMzg1ZjA2NDMyZDY4MDIzYTg0NDVkIn19fQ=="}]}}}},rewardExp:0b,maxUses:9999999}`;
+      } else if (soldItem === "Testa Netherite") {
+        recipeSell = `sell:{id:"minecraft:player_head", Count:${quantitySold}b, tag:{display:{Name:"{\\"text\\":\\"Block of Netherite\\"}"},SkullOwner:{Id:[I;1156551679,-1088599583,-1500868190,-421557710],Properties:{textures:[{Value:"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMjc5NTdmODk1ZDdiYzUzNDIzYTM1YWFjNTlkNTg0YjQxY2MzMGUwNDAyNjljOTU1ZTQ1MWZlNjgwYTFjYzA0OSJ9fX0="}]}}}},rewardExp:0b,maxUses:9999999}`;
+      } else {
+        recipeSell = `sell:{id:"${soldItem}", Count:${quantitySold}b},rewardExp:0b,maxUses:9999999}`;
+      }
+      //---
       if (recipes === "") {
         recipes = recipeBuy + recipeSell;
       } else {
